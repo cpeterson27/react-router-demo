46d2ba8346301fa6d1462b33b5ccfd9cb04324db
CASSANDRA PETERSON <cgdesigns93@gmail.com>
2025-08-23 10:10:13 -0500

    configure font error

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -7,3 +7,5 @@ function AboutMe() {
 }
 
 export default AboutMe;
+
+
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,27 +1,21 @@
+
 import { Routes, Route } from "react-router-dom";
 
-import "./App.css";
 import Header from "../Header/Header";
 import Dashboard from "../Dashboard/Dashboard";
-import Reviews from "../Reviews/Reviews"
-import AboutMe from "../AboutMe/AboutMe"
-<<<<<<< HEAD
-import AboutUs from "../AboutMe/AboutUs"
-=======
->>>>>>> e62781659e733e548f8ed595f2f23126ce5dd2b8
+import Reviews from "../Reviews/Reviews";
+import AboutMe from "../AboutMe/AboutMe";
+import "./App.css";
+
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes>
-<Route path="/" element={<Dashboard />} />
-<Route path="reviews" element={<Reviews />} />
-<Route path="about-me" element={<AboutMe />} />
-<<<<<<< HEAD
-<Route path="about-us" element={<AboutUs />} />
-=======
->>>>>>> e62781659e733e548f8ed595f2f23126ce5dd2b8
+        <Route path="/" element={<Dashboard />} />
+         <Route path="/reviews" element={<Reviews />} />
+          <Route path="/about-me" element={<AboutMe />} />
       </Routes>
     </div>
   );
