b7d094e2fe8613da28db70a4b3135b597b83bc41
CASSANDRA PETERSON <cgdesigns93@gmail.com>
2025-08-24 00:42:59 -0500

    add new pages

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,9 +1,19 @@
 import "./AboutUs.css";
+import { Outlet, Link} from "react-router-dom";
 
 function AboutUs() {
   return (
     <div className="about-us">
+       <ul className="links">
+              <li>
+                <Link to="mission">Mission</Link>
+              </li>
+              <li>
+                <Link to="history">History</Link>
+              </li>
+            </ul>
       <p>You can find out more information about our site here.</p>
+      <Outlet />
     </div>
   );
 }
